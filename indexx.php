170e1f3d97772c3e2704fc2a46a43f61dd1f41e3
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-02-03 15:41:32 +0300

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,25 +1,17 @@
 <?php
-if(!isset($_POST['submit']))
-{
-echo "
-Быстрый конвертер jpeg в gif> </br>
+echo "Быстрый конвертер jpeg в gif .. </br>
 <form method='post' enctype='multipart/form-data'>
 <input type='file' name='image' /></br>
 <input type='submit' name='submit' value='ОК' />
-</form>
-";
-}
+</form>";
 if(isset($_POST['submit']))
 {
-	echo "1";
-if(exif_imagetype($_FILES['image']['tmp_name']) ==  IMAGETYPE_JPEG) 
+$filess = "".$_FILES['image']['tmp_name']."";
+if (exif_imagetype($filess) ==  IMAGETYPE_JPEG)
 {
-	echo "2";
 $rand = rand(0,100000);
 $newpng = "$rand.gif";
-imagegif(imagecreatefromjpeg($_FILES['image']['tmp_name']), $newpng);
+imagegif(imagecreatefromjpeg($filess), $newpng);
+echo "<a href='/$newpng'>$newpng</a>";
 }
-echo "
-<a href='/$newpng'>$newpng</a>
-";
 }

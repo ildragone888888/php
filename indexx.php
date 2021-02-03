b93a92b4ec19632058bc325f0ea56d82e4a506b1
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-02-03 16:14:46 +0300

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,17 +1,21 @@
 <?php
-echo "<form enctype=multipart/form-data action=indexx.php method=POST>
-    <input type=hidden name=MAX_FILE_SIZE value=1000000 />
-    Отправить этот файл: <input name=userfile type=file />
-    <input type=submit name=submit value=Отправить файл />
+if(!isset($_POST['submit']))
+{
+echo "Быстрый конвертер jpeg в gif .. </br>
+<form enctype='multipart/form-data' action='indexx.php' method='POST'>
+<input type='hidden' name='MAX_FILE_SIZE' value='300000' />
+<input name='userfile' type='file' />
+<input type='submit' name='submit' value='Отправить файл' />
 </form>";
+}
 if(isset($_POST['submit']))
 {
-$filess = "".$_FILES['userfile']['tmp_name']."";
-if (exif_imagetype($filess) ==  IMAGETYPE_JPEG)
+if (exif_imagetype($_FILES['userfile']['tmp_name']) ==  IMAGETYPE_JPEG)
 {
 $rand = rand(0,100000);
-$newpng = "$rand.gif";
-imagegif(imagecreatefromjpeg($filess), $newpng);
-echo "<a href='$newpng'>$newpng</a>";
+$newgif = "$rand.gif";
+$res = imagecreatefromjpeg($_FILES['userfile']['tmp_name']);
+header("Content-type: image/gif");
+imagegif($res);
 }
 }

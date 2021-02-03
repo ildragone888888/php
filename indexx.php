f5c23e4d35fd0557e12616ad471b3f8892c41e93
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-02-03 15:49:30 +0300

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,11 +1,17 @@
 <?php
-echo "Быстрый конвертер jpeg в gif .. </br>
-<form method='post' enctype='multipart/form-data'>
-<input type='file' name='image' /></br>
-<input type='submit' name='submit' value='ОК' />
+echo "<form enctype=multipart/form-data action=indexx.php method=POST>
+    <input type=hidden name=MAX_FILE_SIZE value=1000000 />
+    Отправить этот файл: <input name=userfile type=file />
+    <input type=submit name=submit value=Отправить файл />
 </form>";
 if(isset($_POST['submit']))
 {
-$filess = "".$_FILES['image']['tmp_name']."";
- echo $filess;
+$filess = "".$_FILES['userfile']['tmp_name']."";
+if (exif_imagetype($filess) ==  IMAGETYPE_JPEG)
+{
+$rand = rand(0,100000);
+$newpng = "$rand.gif";
+imagegif(imagecreatefromjpeg($filess), $newpng);
+echo "<a href='$newpng'>$newpng</a>";
+}
 }

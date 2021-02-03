d7714fbfaa1dfcc3a8badb433c45350426466ddd
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-02-03 15:26:15 +0300

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -2,7 +2,7 @@
 if(!isset($_POST['submit']))
 {
 echo "
-Быстрый конвертер jpeg в gif </br>
+Быстрый конвертер jpeg в gif >>></br>
 <form method='post' enctype='multipart/form-data'>
 <input type='file' name='image' /></br>
 <input type='submit' name='submit' value='ОК' />
@@ -11,14 +11,13 @@ echo "
 }
 if(isset($_POST['submit']))
 {
-if(exif_imagetype($_FILES['image']['tmp_name']) ==  IMAGETYPE_JPEG) 
+if(exif_imagetype($_FILES['image']['tmp_name']) ==  "IMAGETYPE_JPEG") 
 {
 $rand = rand(0,100000);
-$newpng = "/app/".$rand.gif."";
+$newpng = "/app/".$rand.".gif";
 imagegif(imagecreatefromjpeg($_FILES['image']['tmp_name']), $newpng);
 }
-$fset = file_get_contents("$newpng");
+$fset = file_get_contents($newpng);
 header("Content-type: image/gif");
 echo $fset;
-exit;
 }

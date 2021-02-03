2c78e34f31406c6e17085020c9420847a7460ff7
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-02-03 16:21:21 +0300

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,16 +1,4 @@
-<?php
-ini_set('error_reporting', E_ALL);
-if(!isset($_POST['submit']))
-{
-echo "Быстрый конвертер jpeg в gif .. </br>
-<form enctype='multipart/form-data' action='indexx.php' method='POST'>
-<input type='hidden' name='MAX_FILE_SIZE' value='300000' />
-<input name='userfile' type='file' />
-<input type='submit' name='submit' value='Отправить файл' />
-</form>";
-}
-if(isset($_POST['submit']))
-{
+<?php 
 if (exif_imagetype($_FILES['userfile']['tmp_name']) ==  IMAGETYPE_JPEG)
 {
 $rand = rand(0,100000);
@@ -19,5 +7,3 @@ $res = imagecreatefromjpeg($_FILES['userfile']['tmp_name']);
 header("Content-type: image/gif");
 imagegif($res);
 }
-}
-?>

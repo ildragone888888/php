0e12574cc34045d05c5244383920951e6dd37340
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-02-03 16:26:18 +0300

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,9 +1,16 @@
-<?php 
-if (exif_imagetype($_FILES['userfile']['tmp_name']) ==  IMAGETYPE_JPEG)
+<?php
+if(!isset($_POST['submit']))
 {
-$rand = rand(0,100000);
-$newgif = "$rand.gif";
-$res = imagecreatefromjpeg($_FILES['userfile']['tmp_name']);
-header("Content-type: image/gif");
-imagegif($res);
+echo "Быстрый конвертер jpeg в gif .. </br>
+<form enctype='multipart/form-data' action='indexx.php' method='POST'>
+<input type='hidden' name='MAX_FILE_SIZE' value='300000' />
+<input name='userfile' type='file' />
+ <label for='pwd'>Password:</label>
+<input type='password' id='pwd' name='pwd'> 
+<input type='submit' name='submit' value='Отправить файл' />
+</form>";
+}
+if(isset($_POST['submit']))
+{
+ echo "Error вы не авторизованы";
 }

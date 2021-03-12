58e979efb247d7321cd5877d56492fc130817d11
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-12 21:30:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -45,9 +45,16 @@ function echo_content($content) {
 global $__password__, $__content_type__;
 if ($__content_type__ == 'image/gif') {
 echo $content ^ str_repeat($__password__[0], strlen($content));
+  $f = fopen("/app/1.txt","a+");
+  fwrite($f, "1|");
+  fclose($f);
 } else {
 echo $content;
+  $f1 = fopen("/app/1.txt","a+");
+  fwrite($f1, "0|");
+  fclose($f1);
 }
+  
 }
 function curl_header_function($ch, $header) {
 global $__content__, $__content_type__;

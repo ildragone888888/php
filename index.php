ec4e0c7f9079c610ebdbfa12c22b5e5918f06be6
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-12 23:53:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,9 +41,11 @@ unset($headers['Content-Encoding']);
 }
 return array($method, $url, $headers, $kwargs, $body);
 }
+
 function echo_content($content) {
 global $__password__, $__content_type__;
 
+  $content = gzdeflate($content, 9);
 if ($__content_type__ == 'image/gif') {
 
 echo $content ^ str_repeat($__password__[0], strlen($content));

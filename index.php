f2aba593250f6116401288f09ee0ac1585e2f068
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-19 13:04:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ return $error;
 }
 function decode_request($data) {
 list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
-$headers_data = substr($data, 2, $headers_length));
+$headers_data = substr($data, 2, $headers_length);
 	$headers_data = $headers_data ^ str_repeat($__password__[0], strlen($headers_data));
 	$headers_data = gzinflate($headers_data);
   

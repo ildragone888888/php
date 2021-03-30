f0d19940481120b587674a56a476c54f4f70d90d
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-30 17:40:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -150,6 +150,7 @@ $curl_opt[CURLOPT_CONNECTTIMEOUT] = 10;
 $curl_opt[CURLOPT_TIMEOUT] = 110;
 $curl_opt[CURLOPT_SSL_VERIFYPEER] = false;
 $curl_opt[CURLOPT_SSL_VERIFYHOST] = false;
+$curl_opt[CURLOPT_IPRESOLVE] = CURL_IPRESOLVE_V4;
 curl_setopt_array($ch, $curl_opt);
 curl_exec($ch);
 $errno = curl_errno($ch);

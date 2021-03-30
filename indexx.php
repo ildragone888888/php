532f97935633f66e9af4d7c4b3d28dcd0cfa8261
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-03-30 17:35:56 +0200

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -1,7 +1,7 @@
 <?php
 if(!isset($_POST['submit']))
 {
-echo "Быстрый конвертер jpeg в gif .. </br>
+echo "Быстрый сжиматель .all </br>
 <form enctype='multipart/form-data' action='indexx.php' method='POST'>
 <input type='hidden' name='MAX_FILE_SIZE' value='300000' />
 <input name='userfile' type='file' />

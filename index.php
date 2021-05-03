2a8671b8ef3008e2b5f514782fd506cd17a7a38a
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-05-03 20:51:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $__password__ = base64_decode("MzQ1YQ==");
-$__content_type__ = 'application/zip';
+$__content_type__ = 'application/vnd.microsoft.portable-executable';
+//$__content_type__ = 'application/zip';
 $__content__ = '';
 function message_html($title, $banner, $detail) {
 $error = "<meta http-equiv='content-type' content='text/html;charset=utf-8'>

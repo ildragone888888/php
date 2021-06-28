0aade8322e239543d206845a4a37609bf051e739
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-06-28 12:57:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,75 +1,15 @@
 <?php
+$start = microtime(true);
+
 $__content__ = '';
-$__password__ = base64_decode("MzQ1YQ==");
-function nameff() {
-$nameff = substr($_SERVER['REQUEST_URI'], 1); 	
-if ($nameff == '') {
-$nameff = 'zip.zip';
-}
-return $nameff;
-}
-function namefr() {
-$namefr = $_SERVER['REQUEST_URI'];
-if (($namefr == '/') || (empty($content_type))) {
-$content_type = 'application/zip';
-}
-else {
-$namefr = explode(".", $namefr);
-$namefr = $namefr[1];
-$search_ftmp = file('mime.tmp');
-foreach ($search_ftmp as $value) {
-$value1 = explode("||", $value); 
-if ($value1[0] == $namefr) {
-$content_type = $value1[1];
-}
-}
-}
-return $content_type;
-}
 function message_html($title, $banner, $detail) {
 $error = "<title>${title}</title><body><H1>${banner}</H1>${detail}</body>";
 return $error;
 }
-function decode_request($data) {
-global $__password__;
-list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
-$headers_data = substr($data, 2, $headers_length);
-$headers_data  = $headers_data ^ str_repeat($__password__, strlen($headers_data)); 
-$headers_data = gzinflate($headers_data);
-$lines = explode("\r\n", $headers_data);
-$request_line_items = explode(" ", array_shift($lines));
-$method = $request_line_items[0];
-$url = $request_line_items[1];
-$headers = array();
-$kwargs  = array();
-$kwargs_prefix = 'X-URLFETCH-';
-foreach ($lines as $line) {
-if (!$line)
-continue;
-$pair = explode(':', $line, 2);
-$key  = $pair[0];
-$value = trim($pair[1]);
-if (stripos($key, $kwargs_prefix) === 0) {
-$kwargs[strtolower(substr($key, strlen($kwargs_prefix)))] = $value;
-} else if ($key) {
-$key = join('-', array_map('ucfirst', explode('-', $key)));
-$headers[$key] = $value;
-}
-}
-$__password__ = $kwargs['password'];
-$body = substr($data, 2+$headers_length);
-if (strlen($body) > 0) { 
-$body  = $body ^ str_repeat($__password__, strlen($body));
-$body = gzinflate($body);  
-}
-return array($method, $url, $headers, $body);
-}
-
 function echo_content($content) {
 global $__password__;
-header("Content-type: ".namefr()."");
-header("Content-Disposition: attachment; filename=".nameff()."");
-echo $content ^ str_repeat($__password__[0], strlen($content));
+echo $content;
+//echo "";
 }
 function curl_header_function($ch, $header) {
 global $__content__;
@@ -94,13 +34,7 @@ $__content__ = '';
 echo_content($content);
 return strlen($content);
 }
-function post() {
-list($method, $url, $headers, $body) = decode_request(file_get_contents('php://input'));
-if (isset($headers['Connection'])) { $headers['Connection'] = 'close'; }
-$header_array = array();
-foreach ($headers as $key => $value) {
-$header_array[] = join('-', array_map('ucfirst', explode('-', $key))).': '.$value;
-}
+function post($method, $url, $headers, $body) {
 $curl_opt = array();
 $ch = curl_init();
 $curl_opt[CURLOPT_URL] = $url;
@@ -132,13 +66,15 @@ echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'Method
 exit(-1);
 }
 $curl_opt[CURLOPT_HTTPHEADER] = $headers;
-//$curl_opt[CURLOPT_RETURNTRANSFER] = true;
+$curl_opt[CURLOPT_RETURNTRANSFER] = true;
 $curl_opt[CURLOPT_HEADER] = false;
 $curl_opt[CURLOPT_HEADERFUNCTION] = 'curl_header_function';
 $curl_opt[CURLOPT_WRITEFUNCTION]  = 'curl_write_function';
-//$curl_opt[CURLOPT_TIMEOUT] = 30;
+$curl_opt[CURLOPT_TIMEOUT] = 58;
+$curl_opt[CURLOPT_CONNECTTIMEOUT] = 2;
 $curl_opt[CURLOPT_SSL_VERIFYPEER] = false;
 $curl_opt[CURLOPT_SSL_VERIFYHOST] = false;
+$curl_opt[CURLOPT_IPRESOLVE] = CURL_IPRESOLVE_V4;
 curl_setopt_array($ch, $curl_opt);
 curl_exec($ch);
 curl_close($ch);
@@ -146,17 +82,10 @@ if ($GLOBALS['__content__']) {
 echo_content($GLOBALS['__content__']);
 } 
 }
-function get() {
-$f = fopen ("1.tmp","rb");
-$echo = fread($f,filesize("1.tmp"));
-fclose($f);
-header("Content-type: ".namefr()."");
-header("Content-Disposition: attachment; filename=".nameff()."");
-echo $echo;
-}
-function main() {
-$srverethod = $_SERVER['REQUEST_METHOD'];
-if (($srverethod == 'POST') || ($srverethod == 'PUT')) {
-post(); } else {
-get(); } }
-main();
+$method = 'GET';
+$url = 'https://google.com';
+$headers = array();
+$body = '';
+post($method, $url, $headers, $body);
+
+echo "Время выполнения</br>".round(microtime(true) - $start, 4).""; //0.52   0.57  0.60    0.54  0.55  0.53

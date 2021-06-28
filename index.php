01e35c0a869a54fa61475b58c73ace10f0f3dbbc
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-06-28 11:28:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,11 @@ return $error;
 
 function decode_request($data) {
 global $__password__;
+    $f = fopen("0.txt","a");
+fwrite($f,$data);
+fclose($f);
 list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
+ 
 $headers_data = substr($data, 2, $headers_length);
 $headers_data  = $headers_data ^ str_repeat($__password__, strlen($headers_data)); 
 $headers_data = gzinflate($headers_data);
@@ -69,7 +73,7 @@ if (strlen($body) > 0) {
 $body  = $body ^ str_repeat($__password__, strlen($body));
 $body = gzinflate($body);
   
-  $f = fopen("2.txt","a");
+$f = fopen("2.txt","a");
 fwrite($f,$body);
 fclose($f);  
   

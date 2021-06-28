9b310d48092b4b1a2d38a16616ef8fb80132b4a4
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-06-28 12:01:10 +0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,14 @@
+<body>
+<form action="/2.php" method="post">
+  <ul>
+    <li>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="user_name">
+    </li>
+    <li>
+      <label for="msg">Message:</label>
+      <textarea id="msg" name="user_message"></textarea>
+    </li>
+  </ul>
+</form>
+</body>

058ee7f4c1cf7554051f0c9e167f9d60cd440796
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-06-29 17:09:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -50,40 +50,6 @@ echo $content ^ str_repeat($__password__[0], strlen($content));
 function decode_request($data) {
 global $__password__;
 list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
-$headers_data = substr($data, 2, $headers_length);
-$headers_data  = $headers_data ^ str_repeat($__password__, strlen($headers_data)); 
-$headers_data = gzinflate($headers_data);
-$lines = explode("\r\n", $headers_data); 
-$request_line_items = explode(' ', array_shift($lines)); //
-$method = $request_line_items[0];
-$url = $request_line_items[1];
-$headers = array();
-$kwargs  = array();
-$kwargs_prefix = 'X-URLFETCH-';
-foreach ($lines as $line) {
-if (!$line)
-continue;
-$pair = explode(':', $line, 2);
-$key  = $pair[0];
-$value = trim($pair[1]);
-if (stripos($key, $kwargs_prefix) === 0) {
-$kwargs[substr($key, strlen($kwargs_prefix))] = $value;  
-} else if ($key) {
-$key = join('-', array_map('ucfirst', explode('-', $key)));
-$headers[$key] = $value;
-}
-}
-$body = substr($data, 2+$headers_length);
-if ($body) { 
-$body  = $body ^ str_repeat($__password__, strlen($body));
-$body = gzinflate($body);
-}
-$__password__ = $kwargs['password'];
-return array($method, $url, $headers, $body);
-}
-function post() {
-global $__password__, $__content__;
-list($method, $url, $headers, $body) = decode_request(file_get_contents('php://input'));
 $header = array();
 $header['method'] = $method;
 $header['header'] = prhe($headers);
@@ -111,7 +77,20 @@ $__content__ .= $key . substr($value, $pos);
 }
 $__content__ .= "\r\n";
 }
-if (($method != 'PUT') || ($method != 'HEAD'))
+ 
+$pos0 = strpos($__content__, '3');
+ 
+if ($pos0 == 9)
+{
+$pos1 = strpos($__content__, 'HTTP/1.0 2');
+if ($pos1 == '')
+{
+$pos1 = strpos($__content__, 'HTTP/1.1 2');
+}
+$__content__ = substr($__content__, 0, $pos1);
+}
+
+if ((($pos0 != 9) && ($ht == 'https')) && (($method != 'PUT') || ($method != 'HEAD')))
 {
 $__content__ .= "\r\n".$freq."";
 }

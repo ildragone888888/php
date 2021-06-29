bf6e1705e876d287cdcaf6eb2b7077b70ccc4b7e
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-06-29 16:30:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$__content__ = '';
 function prhe($headers) {
 $flattened = array();
 foreach ($headers as $key => $header) {
@@ -16,15 +17,65 @@ $flattened[] = $key.': '.$header;
 }
 return implode("\r\n", $flattened);
 }
+function namef() {
+$req = $_SERVER['REQUEST_URI'];
+if (($req == '/') || ($req == '')) {
+$nameff = 'zip.zip';
+$namefr = 'application/zip';
+}
+else {
+$nameff = str_replace('/', '', $req);
+$namefr = substr($req, 1); 
+$namefr = explode('.', $namefr);
+$namefr = $namefr[1];
+$tmp = file('mime.tmp');
+foreach ($tmp as $key) {
+$key = explode('||', $key); 
+if ($key[0] == $namefr) {
+$namefr = $key[1];
+}
+}
+}
+return array($nameff, $namefr);
+}
+$__password__ = base64_decode('MzQ1YQ==');
+function decode_request($data) {
+global $__password__;
+list($headers_length) = array_values(unpack('n', substr($data, 0, 2)));
+$headers_data = substr($data, 2, $headers_length);
+$headers_data  = $headers_data ^ str_repeat($__password__, strlen($headers_data)); 
+$headers_data = gzinflate($headers_data);
+$lines = explode("\r\n", $headers_data); 
+$request_line_items = explode(' ', array_shift($lines)); //
+$method = $request_line_items[0];
+$url = $request_line_items[1];
+$headers = array();
+$kwargs  = array();
+$kwargs_prefix = 'X-URLFETCH-';
+foreach ($lines as $line) {
+if (!$line)
+continue;
+$pair = explode(':', $line, 2);
+$key  = $pair[0];
+$value = trim($pair[1]);
+if (stripos($key, $kwargs_prefix) === 0) {
+$kwargs[substr($key, strlen($kwargs_prefix))] = $value;  
+} else if ($key) {
+$key = join('-', array_map('ucfirst', explode('-', $key)));
+$headers[$key] = $value;
+}
+}
+$body = substr($data, 2+$headers_length);
+if ($body) { 
+$body  = $body ^ str_repeat($__password__, strlen($body));
+$body = gzinflate($body);
+}
+$__password__ = $kwargs['password'];
+return array($method, $url, $headers, $body);
+}
 
-
-$__content__ = '';
-
-
-
-function post($method, $url, $headers, $body)
-{
-global $__content__;
+function echo_content($content) {
+global $__password__;
 $header = array();
 $header['method'] = $method;
 $header['header'] = prhe($headers);
@@ -56,10 +107,22 @@ if (($method != 'PUT') || ($method != 'HEAD'))
 {
 $__content__ .= "\r\n".$freq."";
 }
+if ($__content__) {
+echo_content($__content__);
 }
-$method = "GET";
-$url = "https://yandex.ru";
-$headers = array();
-$body = 'user_name=alex&user_message=text';
-post($method, $url, $headers, $body);
-echo $__content__;
+}
+function get() {
+$f = fopen ('1.tmp','rb');
+$echo = fread($f,filesize('1.tmp'));
+fclose($f);
+list($nameff, $namefr) = namef();
+header('Content-type: '.$namefr.'');
+header('Content-Disposition: attachment; filename='.$nameff.'');
+echo $echo;
+}
+function main() {
+$shod = $_SERVER['REQUEST_METHOD'];
+if (($shod == 'POST') || ($shod == 'PUT')) {
+post(); } else {
+get(); } }
+main();

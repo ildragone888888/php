49476cb0b8a1a89b611ce2bdc118522ec77e5b62
ildragone888888 <48631634+ildragone888888@users.noreply.github.com>
2021-06-30 13:34:38 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,15 +1,18 @@
+<?php
+echo "
 <body>
-<form action="/2.php" method="post">
+<form action='/2.php' method='post'>
   <ul>
     <li>
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="user_name">
+      <label for='name'>Name:</label>
+      <input type='text' id='name' name='user_name'>
     </li>
     <li>
-      <label for="msg">Message:</label>
-      <textarea id="msg" name="user_message"></textarea>
+      <label for='msg'>Message:</label>
+      <textarea id='msg' name='user_message'></textarea>
     </li>
   </ul>
-  <input type="submit" value="Выбрать">
+  <input type='submit' value='Выбрать'>
 </form>
 </body>
+";
